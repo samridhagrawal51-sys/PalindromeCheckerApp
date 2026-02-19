1cae55dba84a6e952a5f942a4deb5f5417f6006b
samridhagrawal51-sys <samridhagrawal51-sys>
2026-02-19 10:21:25 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,7 +1,14 @@
 public class PalindromeChecker {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version:1.0");
-        System.out.println("System initialized successfully");
+        String text="madam";
+        boolean isPalindrome=true;
+        for (int i=0;i<text.length()/2;i++) {
+            if (text.charAt(i)!=text.charAt(text.length()-1-i)) {
+                isPalindrome=false;
+                break;
+            }
+        }
+        System.out.println("Input text:"+text);
+        System.out.println("Is it Palindrome?: "+isPalindrome);
     }
 }
