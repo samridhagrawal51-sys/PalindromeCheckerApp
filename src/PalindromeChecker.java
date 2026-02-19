49f0cd99192b9f2b9a11133963a14a956412d0f0
samridhagrawal51-sys <samridhagrawal51-sys>
2026-02-19 11:14:31 +0530

    UC3:Hardcoded palindrome

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,14 +1,18 @@
+import java.util.Scanner;
 public class PalindromeChecker {
     public static void main(String[] args) {
-        String text="madam";
-        boolean isPalindrome=true;
-        for (int i=0;i<text.length()/2;i++) {
-            if (text.charAt(i)!=text.charAt(text.length()-1-i)) {
-                isPalindrome=false;
-                break;
-            }
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a string:");
+        String original=sc.nextLine();
+        String reversed="";
+        for (int i=original.length()-1;i>=0;i--) {
+            reversed=reversed+original.charAt(i);
         }
-        System.out.println("Input text:"+text);
-        System.out.println("Is it Palindrome?: "+isPalindrome);
+        if (original.equals(reversed)) {
+            System.out.println("The given string is a palindrome.");
+        }else {
+            System.out.println("The given string is not a palindrome.");
+        }
+        sc.close();
     }
 }
