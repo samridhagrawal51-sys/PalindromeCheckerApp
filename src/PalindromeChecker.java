ca29209dd723b43e78f5cb6b9ae299a43ec65017
samridhagrawal51-sys <samridhagrawal51-sys>
2026-02-19 10:27:33 +0530

    UC1:Add welcome message module

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,14 +1,7 @@
 public class PalindromeChecker {
     public static void main(String[] args) {
-        String text="madam";
-        boolean isPalindrome=true;
-        for (int i=0;i<text.length()/2;i++) {
-            if (text.charAt(i)!=text.charAt(text.length()-1-i)) {
-                isPalindrome=false;
-                break;
-            }
-        }
-        System.out.println("Input text:"+text);
-        System.out.println("Is it Palindrome?: "+isPalindrome);
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version:1.0");
+        System.out.println("System initialized successfully");
     }
 }
